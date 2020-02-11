c110b2e92270becd95b683e7d3a9469dc2f4ca8c
Aleksei <a.gryczewski999@gmail.com>
2020-02-11 14:53:44 -0500

    Skeleton

--- src/main/java/dev/salmonllama/snack/Main.java
+++ src/main/java/dev/salmonllama/snack/Main.java
@@ -1,4 +1,25 @@
 package dev.salmonllama.snack;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent rootNode = FXMLLoader.load(getClass().getResource("/scenes/SnackScene.fxml"));
+        Scene scene = new Scene(rootNode);
+
+        stage.setTitle("ITP100 - W7 - Snack");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
--- src/main/java/dev/salmonllama/snack/scenes/SnackController.java
+++ src/main/java/dev/salmonllama/snack/scenes/SnackController.java
@@ -1,7 +1,9 @@
 package dev.salmonllama.snack.scenes;
 
+import dev.salmonllama.snack.Constants;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -18,8 +20,18 @@ public class SnackController implements Initializable {
     @FXML
     private TextField drinksField;
 
+    @FXML
+    private Label chipsLabel;
+
+    @FXML
+    private TextField chipsField;
+
+    @FXML
+    private Button calcButton;
+
     @Override
     public void initialize(URL url, ResourceBundle bundle) {
-
+        String prices = String.format("Drinks are %.2f each. Chips are %.2f each.", Constants.DRINK_PRICE, Constants.CHIP_PRICE);
+        pricesLabel.setText(prices);
     }
 }
